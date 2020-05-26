2ce3ded6e305b2fbc4739b211a95e162df3cd528
Stephen Philp <stephen.philp@clio.com>
2020-05-26 15:19:18 -0700

    new enemy every once in a while

--- app/main.rb
+++ app/main.rb
@@ -1,16 +1,53 @@
 require "lib/player_sprite.rb"
 require "lib/enemy_sprite.rb"
+require "lib/foodie_sprite.rb"
 
 def tick(args)
+  args.outputs.sounds << "sounds/background.wav" if args.tick_count == 0
+
   setup_game(args)
 
+  return if args.state.reset_count >= args.tick_count
+
+  if args.tick_count >= args.state.next_enemy
+    args.outputs.sounds << "sounds/background.wav"
+    args.state.enemies << EnemySprite.new(args)
+    args.state.next_enemy += 1000
+  end
+
+
+  if args.inputs.keyboard.key_held.space
+    if args.state.player.attitude == "run"
+      args.state.player.attitude = "attack"
+      args.state.player.path = 'sprites/buggy_attack.png'
+      puts "ATTACK"
+    elsif args.state.player.attitude == "attack"
+      args.state.player.attitude = "run"
+      args.state.player.path = 'sprites/buggy.png'
+      puts "RUN"
+    end
+  end
   calculate_sprite_speeds(args)
-  move_sprites(args)
+  args.state.enemies.each do |enemy|
+    if enemy.speed_xy >=5 || enemy.speed_up_down >= 5
+      # args.outputs.sounds << "sounds/lunge.wav"
+    end
+  end
+  player_collission = check_player_collission(args)
   player_out = check_bounds(args)
-  reset(args) if player_out
+  poor_enemies = check_enemy_collissions(args.state.enemies)
+  poor_enemies.each do |pe|
+    pe.rotation *= -1
+    args.outputs.sounds << "sounds/whup.wav"
+  end
+
+  move_sprites(args)
+  reset(args) if player_out || player_collission
 end
 
 def setup_game(args)
+  args.state.reset_count ||= 0
+  args.state.next_enemy ||= 1000
   setup_player(args)
   setup_enemies(args)
 end
@@ -20,7 +57,7 @@ def setup_player(args)
 end
 
 def setup_enemies(args)
-  args.state.enemies ||= 10.map { EnemySprite.new(args.outputs) }
+  args.state.enemies ||= 2.map { EnemySprite.new(args.outputs) }
 end
 
 def reset(args)
@@ -29,7 +66,36 @@ def reset(args)
   args.state.enemies = nil
 
   screams = %w[scream1.wav]
-  args.outputs.sounds << "sounds/#{screams.sample}"
+  # args.outputs.sounds << "sounds/#{screams.sample}"
+  args.state.reset_count = args.tick_count + 100
+  args.state.next_enemy = args.tick_count + 1000
+end
+
+def check_player_collission(args)
+  collided = false
+  args.state.enemies.each do |enemy|
+    enemy.register_collision -= 1 if enemy.register_collision > 1
+    if enemy.rect.intersect_rect?(args.state.player.rect)
+      collided = true if enemy.register_collision < 1
+      enemy.register_collision = 1200
+      # puts "wrecked #{args.state.player.rect} #{enemy.rect}"
+    end
+  end
+  collided
+end
+
+def check_enemy_collissions(enemies)
+  collided_enemies = []
+  enemies.each do |enemy|
+    next if collided_enemies.include?(enemy)
+    enemies.each do |check|
+      next if enemy == check
+      if enemy.rect.intersect_rect? check.rect
+        collided_enemies << check
+      end
+    end
+  end
+  collided_enemies
 end
 
 def check_bounds(args)
@@ -48,5 +114,6 @@ def move_sprites(args)
   args.state.player.move!
   args.state.enemies.each do |enemy|
     enemy.move!
+    enemy.angle = (args.state.tick_count % enemy.rotation_speed) * enemy.rotation
   end
 end
--- lib/player_sprite.rb
+++ lib/player_sprite.rb
@@ -1,33 +1,50 @@
 class Player
   attr_sprite
   attr_accessor :speed_xy, :speed_up_down, :previous_key_xy, :previous_key_up_down
-  attr_accessor :x, :y, :w, :h
+  attr_accessor :x, :y, :w, :h, :attitude, :r, :g, :b, :a, :path
 
   def initialize(outputs)
     @x = rand * 1260
-    @y = rand * 700
+    @y = (rand * 200)
     @w = 32 # size
     @h = 32
+    # @r = 255
+    # @g = 0
+    # @b = 0
+    @a = 255
     @speed_xy = 0
     @speed_up_down = 0
     @path = 'sprites/buggy.png'
     @previous_key_xy = nil
     @previous_key_up_down = nil
+    @attitude = "run"
     outputs.static_sprites << self
+  end
+
+  def serialize
+    {}
+  end
+
+  def inspect
+    serialize.to_s
+  end
+
+  def to_s
+    serialize.to_s
   end
 
   def calculate_speed(key_held)
     if key_held.right
-      @speed_xy += 0.5 unless @speed_xy > 10
+      @speed_xy += 0.7 unless @speed_xy > 6
       @previous_key_xy = "right"
     elsif key_held.left
-      @speed_xy -= 0.5 unless (@speed_xy * -1) > 10
+      @speed_xy -= 0.7 unless (@speed_xy * -1) > 6
       @previous_key_xy = "left"
     elsif key_held.up
-      @speed_up_down += 0.5 unless @speed_up_down > 10
+      @speed_up_down += 0.9 unless @speed_up_down > 6
       @previous_key_up_down = "up"
     elsif key_held.down
-      @speed_up_down -= 0.5 unless (@speed_up_down * -1) > 10
+      @speed_up_down -= 0.9 unless (@speed_up_down * -1) > 6
       @previous_key_up_down = "down"
     end
   end
@@ -46,5 +63,13 @@ class Player
     if @previous_key_up_down == "down"
       @y += @speed_up_down unless @y <= -30
     end
+  end
+
+  def rect
+    [@x, @y, @w / 2, @h / 2]
+  end
+
+  def x_y
+    [@x, @y]
   end
 end
